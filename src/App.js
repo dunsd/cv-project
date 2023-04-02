1b58a3fd6c2a048b5336f28a5099c54cb587ca2a
dunsd <dthomasparker@gmail.com>
2023-04-02 23:01:36 +0100

    End of day commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+
+  }
+
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
